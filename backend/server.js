221db5f2d2be15a6181ebdf86d897c6760e159d7
VirajDeorukhakar123 <70628669+VirajDeorukhakar123@users.noreply.github.com>
2025-11-18 14:46:33 +0530

    Used mysql2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const path = require('path'); // Import the 'path' module
 const app = express();
 const port = 3000;
